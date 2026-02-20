ac848046332628b3254efe4a635e5c2bb92e6333
Alexandre <alexandredothennequin@gmail.com>
2026-02-20 11:29:08 +0100

    Fix cosine similarity function for 3 dimensions matrixes

--- src/metrics/cosine_similarity.py
+++ src/metrics/cosine_similarity.py
@@ -6,11 +6,17 @@ from src.metrics.metric_interface import MetricInterface
 class CosineSimilarity(MetricInterface):
     def compute(self, frame_embedding_a, frame_embedding_b) -> float:
         """Compute the cosine similarity between two frame embeddings."""
-        # Compute cosine similarity between the two frame embeddings
-        dot_product = np.dot(frame_embedding_a, frame_embedding_b)
-        norm_frame_a = np.linalg.norm(frame_embedding_a)
-        norm_frame_b = np.linalg.norm(frame_embedding_b)
-        if norm_frame_a == 0 or norm_frame_b == 0:
-            return 0
-        cosine_similarity = dot_product / (norm_frame_a * norm_frame_b)
-        return cosine_similarity
+        normalized_embeddings_a = frame_embedding_a / (
+            np.linalg.norm(frame_embedding_a, axis=-1, keepdims=True) + 1e-8
+        )
+        normalized_embeddings_b = frame_embedding_b / (
+            np.linalg.norm(frame_embedding_b, axis=-1, keepdims=True) + 1e-8
+        )
+
+        cosine_similarity = np.einsum(
+            "bnd,bmd->bnm",
+            normalized_embeddings_a,
+            normalized_embeddings_b,
+        )
+
+        return cosine_similarity.mean()
